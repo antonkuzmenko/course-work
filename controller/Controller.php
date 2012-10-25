b5c21392409cc1d992596c30b4500f00c4f95374
Anton Kuzmenko <antonkuzmenko.dev@gmail.com>
2012-10-26 01:04:27 +0300

    twig integration

--- controller/Controller.php
+++ controller/Controller.php
@@ -1,6 +1,27 @@
 <?php
 
 class Controller {
+  private $twig;
+
+  public function __construct() {
+    $this->loadTwig();
+    echo $this->render('index', array());
+  }
+
+  /**
+   * Load and create Twig object.
+   */
+  private function loadTwig() {
+    include_once('twig/lib/Twig/Autoloader.php');
+    Twig_Autoloader::register();
+
+    $loader = new Twig_Loader_Filesystem('templates');
+    $this->twig = new Twig_Environment($loader, array(
+      'cache' => 'cache',
+      'debug' => TRUE,
+    ));
+    $this->twig->addExtension(new Twig_Extension_Core());
+  }
 
   /**
    * Render template.
@@ -13,15 +34,32 @@ class Controller {
    * @return bool
    *  Returns true if template exists
    */
-  public function render($path, $args = array()) {
+  public function render($template, $args = array()) {
+    return $this->twig->render($template . '.html.twig', $args);
+  }
 
-    if ( file_exists($path) ) {
-      extract($args, EXTR_OVERWRITE);
-      file_get_contents($path);
-      return TRUE;
+  /**
+   * Clear templates cache.
+   */
+  protected function clearCache() {
+    $this->twig->clearCacheFiles();
+  }
+
+  /**
+   * Get url argument.
+   *
+   * @param int $n
+   *  Position of the url argument
+   *
+   * @return string
+   *  argument or empty string
+   */
+  protected function arg($n = 0) {
+    list($args) = explode('/', $_SERVER['PATH_INFO']);
+    if (isset($args[$n]) && !empty($args[$n])) {
+      return $args[$n];
     }
 
-    return FALSE;
+    return '';
   }
 }
-
--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php
 include_once('controller/Controller.php');
 $controller = new Controller();
-
-$controller->render('templates/html.tpl.php', array());
